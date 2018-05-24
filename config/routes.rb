c2325aad60f17067066069190733583378ed054f
akihiro.ishii <ishii.akihiro@gmail.com>
2018-05-24 12:22:31 +0000

    make function login

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
-  get 'users/index'
+  get 'sessions/new'
 
-  get 'users/new'
+  get 'sessions/create'
 
-  get 'users/show'
-
-  get 'users/create'
+  get 'sessions/destroy'
 
   root to: 'toppages#index' #ログイン機能追加のため、トップページをrootに変更
   #root to: 'tasks#index'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
